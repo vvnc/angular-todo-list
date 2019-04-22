fd6c0d31fe419ffcfc60d9350125c694a165aa48
Tsagana Nokhaeva <nokhaevatsagana@gmail.com>
2019-04-22 21:46:53 +0300

    Add lorem ipsum generated content

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,13 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { LoremIpsum } from "lorem-ipsum";
+
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+  private static lorem: LoremIpsum = new LoremIpsum();
+  private todoItems: string[];
 
-  constructor() { }
+  constructor() {
+    const length = Math.floor(Math.random() * 10);
+    this.todoItems = new Array(length);
+    for (let i = 0; i < length; i++) {
+      this.todoItems[i] = CardComponent.lorem
+        .generateWords(Math.floor(Math.random() * 20));
+    }
+
+    CardComponent.lorem.generateWords(1);
+  }
 
   ngOnInit() {
   }
